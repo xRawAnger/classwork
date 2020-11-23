cf3d92e965ed2a30836fe36e7bee83c27e87ff03
MrDarkG <vako.batiashvili1998@gmail.com>
2020-11-23 18:30:48 +0400

    register controller

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('nickname')->unique();
+            $table->string('phone_num');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
